3b949dd56b21eccf9644d326ec056a5816c6b899
Alexandre Perdomo <aperdomo@free.fr>
2021-06-30 17:53:44 +0200

    app change for possible prediction

--- api/app.py
+++ api/app.py
@@ -1,5 +1,6 @@
 
 from fastapi import FastAPI
+import joblib
 
 app = FastAPI()
 
@@ -7,7 +8,38 @@ app = FastAPI()
 # define a root `/` endpoint
 @app.get("/")
 def index():
-    return {"ok": True}
+	return {"ok": True}
 
 
 # Implement a /predict endpoint
+@app.get("/predict")
+def index(acousticness,danceability,duration_ms,energy,
+		  explicit,id,instrumentalness,key,liveness,loudness,
+		  mode,name,release_date,speechiness,tempo,valence,artist):
+	data = dict(
+        acousticness=float(acousticness),
+        danceability=float(danceability),
+        duration_ms=int(duration_ms),
+        energy=float(energy),
+        explicit=int(explicit),
+        id=id, 
+        instrumentalness=float(instrumentalness), 
+        key = int(key), 
+        liveness= float(liveness),
+        loudness = float(loudness), 
+        mode=int(mode), 
+        name=name,
+        release_date= release_date,
+        speechiness=float(speechiness), 
+        tempo=float(tempo),
+        valence=float(valence),
+        artist=artist
+	)
+	model = joblib.load('model.joblib')
+	popularity = model.predict(data)
+
+	return {
+		"artist": artist,
+		"name": name,
+		"popularity": popularity,
+	}
